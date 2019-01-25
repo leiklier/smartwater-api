50a42e4c905cb1af042caf49a9d8e8d4e17692ba
Stens <kris.ste@online.no>
2019-01-25 17:11:37 +0100

    Update server.js
    Added database functionality

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+// https://stackoverflow.com/questions/40730604/mongoose-find-with-multiple-ids-that-are-the-same?rq=1
+// https://stackoverflow.com/questions/36552395/query-multiple-date-ranges-in-mongoose
+
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const api = express()
@@ -6,13 +10,20 @@ const port = process.env.PORT || 5000
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-mongoose.connect('mongodb://localhost:27017/vannovervakning', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/vannovervakning', {useNewUrlParser:true})
 
 const sensorReadingSchema = new Schema({
-  id: Number,
+  _id: Number,
   nodeId: Number,
   type: String,
-  value: Decimal128
+  value: Number,
+  time: {
+    type: Date,
+    default: Date.now,
+    required: "rewortwe"
+  }
+}, {
+  collection: "readings"
 })
 
 var sensorReading = mongoose.model('sensorReading', sensorReadingSchema)
@@ -31,21 +42,56 @@ var server = api.listen(port, (err) => {
 })
 
 
+
 // ROUTES
 
 api.get('/api/node/:nodeId?', (req, res) => {
   // Get info about this node
-  res.send(`You request info about node #${req.params.nodeId}`)
+  // Returns all entries on that node
+  sensorReading.find({nodeId: req.params.nodeId}, function(err, node) {
+    if (err) {
+        res.send(err);
+    }
+    // console.log(node);
+    res.json(node);
+    });
 })
 
 
 
-api.get('/api/sensorReadings/:nodeId/:param/:fromTimestamp?/:toTimestamp?', (req, res) => {
+
+api.get('/api/node/:nodeId?/:param?/:fromTimestamp?/:toTimestamp?', (req, res) => {
   // GET sensor readings
-  // No timestamps specified returns the latest reading
-  res.send(req.params)
+  // No timestamps specified returns the latest reading.
+
+// Possible solution
+  sensorReading.find({nodeId: req.params.nodeId,
+    $or: [
+      { // range 1
+        time: { '$gte': new Date(req.params.fromTimestamp), '$lt': new Date(req.params.toTimestamp) }
+      },
+    ]
+    }, function(err, node){
+    if (err) {
+      res.send(err);
+    }
+    res.json(node);
+    });
 })
+
+
 
 api.post('/api/sensorReadings/:nodeId', (req, res) => {
-  res.send(req.params)
+  const reading = new sensorReading(req.body)
+  reading.save()
+    .then(reading=> {
+      res.json('reading stored securely in database')
+    })
+    .catch(err => {
+      res.status(400).send('unable to store reading')
+    })
 })
+
+
+
+
